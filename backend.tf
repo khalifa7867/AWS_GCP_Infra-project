1514de705f74d45942a7b4edcc523b67794da924
khalifa7867 <shiakhussiankhalifa@gmail.com>
2026-01-13 04:01:56 +0530

    Remove AWS profile from backend config for GitHub Actions

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "my-terraform-awsxgcp-state-bucket"
     key    = "aws_gcp_infra_provisioning-project/terraform.tfstate"
     region = "ap-south-1"
-    profile = "ap-south"
+    # profile = "ap-south"  # Commented out for GitHub Actions - uses env vars
     use_lockfile = true
   }
 }
